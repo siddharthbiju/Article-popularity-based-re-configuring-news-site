cbb59cb7089c4b4096958ce55399373674167fc2
siddharthbiju <siddharthbiju@gmail.com>
2019-09-16 07:48:58 +0530

    modified code

--- twittertrending.py
+++ twittertrending.py
@@ -6,12 +6,17 @@ from newsplease import NewsPlease
 
 def print_news(url):
     article = NewsPlease.from_url(url)
-    print(article.title)
-    print("\n")
-    print(article.text)
-    print("\n")
-    print(article.url)
-    print("\n")
+    tem = article.text
+    res = len(tem.split())
+    r = int(res)
+    if r > 400:
+        news = news+1
+        print(article.title)
+        print("\n")
+        print(article.text)
+        print("\n")
+        print(article.url)
+        print("\n")
 
 
 def my_function(query):
@@ -24,6 +29,7 @@ def bash_command(cmd):
     subprocess.Popen(cmd, shell=True, executable='/bin/bash')
 
 
+news = 0
 print("sample news feed by siddharth biju")
 print("crawling twitter...")
 bash_command(
@@ -36,21 +42,21 @@ f.close
 keyarray = []
 links = []
 data = json.loads(data)
-for i in range(10):
+for i in range(15):
     temp = data[0]["trends"][i]["name"]
-    if temp == "#":
+    if temp[0] == "#":
         temp = temp[1:]
     temp = temp + " latest news articles"
     keyarray.append(temp)
-    #print(keyarray[i])
+
 
 print("searching for news links in google....")
 
-for i in range(10):
+for i in range(15):
     que = keyarray[i]
     temp = my_function(que)
     links.append(temp)
 
-for i in range(10):
+for i in range(15):
     print("crawling and extracting data...")
     print_news(links[i])
