5afbf5bb822c04cedc926bc4ed95e905fcab5101
siddharthbiju <siddharthbiju@gmail.com>
2019-10-15 23:09:26 +0530

    location

--- web/news/src/jsons/thenews.jsx
+++ web/news/src/jsons/thenews.jsx
@@ -1,25 +1,35 @@
 import React from "react";
 import PostData from "./new.json";
-import "./border.css"
+import "./border.css";
 
 class News extends React.Component {
   render() {
     const ll = this.props.coun;
-
+    const location = this.props.lo;
     return (
       <div>
         {PostData.map((postDetail, index) => {
-          if (index === ll)
+          if (index === ll && location === "Global")
             return (
               <div key={index}>
                 <h1 className="bor">
-                <h2 className="p">{postDetail.title}</h2>
-                <p>{postDetail.content}</p>
-                <a href={postDetail.url}>{postDetail.url}</a>
+                  <h2 className="p">{postDetail.title}</h2>
+                  <p>{postDetail.content}</p>
+                  <a href={postDetail.url}>{postDetail.url}</a>
                 </h1>
               </div>
             );
-        })} 
+          else if (index === ll && postDetail.loc == location)
+            return (
+              <div key={index}>
+                <h1 className="bor">
+                  <h2 className="p">{postDetail.title}</h2>
+                  <p>{postDetail.content}</p>
+                  <a href={postDetail.url}>{postDetail.url}</a>
+                </h1>
+              </div>
+            );
+        })}
       </div>
     );
   }
