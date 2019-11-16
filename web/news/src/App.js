0ecc28489c257a0984cd4ddabcb087be9343618e
siddharthbiju <siddharthbiju@gmail.com>
2019-11-16 21:34:23 +0530

    Dynamic design

--- web/news/src/App.js
+++ web/news/src/App.js
@@ -1,27 +1,57 @@
 import React from "react";
+import Top from "./components/jsons/topnews";
 import News from "./components/jsons/thenews";
 import Navb from "./components/navbar";
+
 import "./App.css";
 import { Helmet } from "react-helmet";
 
 class App extends React.Component {
-  state = { count: 10, location_current: "Global" };
+  state = { count: 10, location_current: "Global", showPopup: false };
 
   set_loc = set => {
     this.setState({ location_current: set });
   };
+
   render() {
-    const list = [];
-    for (var i = 0; i < this.state.count; i++) {
-      list.push(<News coun={i} lo={this.state.location_current} />);
+    const list1 = [];
+    const list2 = [];
+    var i;
+    for (i = 2; i < this.state.count; i++) {
+      list2.push(
+        <News
+          coun={i}
+          lo={this.state.location_current}
+          popup={this.togglePopup}
+        />
+      );
+    }
+    for (i = 0; i < 2; i++) {
+      list1.push(
+        <Top
+          coun={i}
+          lo={this.state.location_current}
+          popup={this.togglePopup}
+        />
+      );
     }
     return (
       <div>
         <Helmet>
+          <title>Trending News</title>
           <style>{"body { background-color: #fafafa; }"}</style>
         </Helmet>
         <Navb loo={this.set_loc} lo={this.state.location_current} />
-        {list}
+        <div className="trend">
+          <p>Top stories</p>
+        </div>
+
+        <div className="nn">{list1}</div>
+
+        <div className="latest">
+          <p>Latest news</p>
+        </div>
+        {list2}
       </div>
     );
   }
--- web/news/src/components/jsons/topnews.jsx
+++ web/news/src/components/jsons/topnews.jsx
@@ -2,19 +2,26 @@ import React from "react";
 import PostData from "./new.json";
 import "./border.css";
 
-class News extends React.Component {
+class Top extends React.Component {
   state = { showPopup: false };
 
   render() {
     const ll = this.props.coun;
     const location = this.props.lo;
+    var cl;
+    if (ll === 0) {
+      cl = "wrap2";
+    } else {
+      cl = "wrap3";
+    }
+
     return (
       <div>
         {PostData.map((postDetail, index) => {
           if (index === ll && location === "Global")
             return (
               <div key={index}>
-                <div className="wrap">
+                <div className={cl}>
                   <h2 className="p">{postDetail.title}</h2>
                   <p>{postDetail.content}</p>
                   <a href={postDetail.url}>Visit website -></a>
@@ -24,7 +31,7 @@ class News extends React.Component {
           else if (index === ll && postDetail.loc === location)
             return (
               <div key={index}>
-                <div className="wrap">
+                <div className={cl}>
                   <h2 className="p">{postDetail.title}</h2>
                   <p>{postDetail.content}</p>
                   <a href={postDetail.url}>Visit website -></a>
@@ -38,4 +45,4 @@ class News extends React.Component {
   }
 }
 
-export default News;
+export default Top;
